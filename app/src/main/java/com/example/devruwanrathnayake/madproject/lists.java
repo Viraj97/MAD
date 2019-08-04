e11350d1042a50d4889221cf81bde59fd59b5563
Viraj Devruwan <rathnayaka.viraj@gmail.com>
2019-08-05 02:08:57 +0530

    splash screen, home page, lists page done 05/08/2019 02:08AM

---
+++ app/src/main/java/com/example/devruwanrathnayake/madproject/lists.java
@@ -0,0 +1,13 @@
+package com.example.devruwanrathnayake.madproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lists);
+    }
+}
